2d0e1f388891e8399b19656bb3ee8f50b1f720e8
qilinjie <qilinjie1991@outlook.com>
2020-09-28 17:34:05 +0800

    feat: monoca editor

--- src/views/charts/monoca.vue
+++ src/views/charts/monoca.vue
@@ -1,16 +1,38 @@
 <template>
   <el-container class="monoca-container">
-    monoca
+    <el-main id="monoca" class="monoca-main"></el-main>
   </el-container>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
+import { Vue, Component } from 'vue-property-decorator'
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.main.js'
+import { component } from 'vue/types/umd'
 
-})
+@Component
+export default class Chart extends Vue {
+  public monoca
+  public str = 'hello world'
+
+  created () {
+    // this.monoca = monaco.editor.create(
+    //   document.querySelector('#monoca'),
+    //   {
+    //     value: `console.log(${this.str})`,
+    //     language: 'typescript'
+    //   }
+    // )
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-
+  .monoca-container {
+    width: 100%;
+    height: 100%;
+    .monoca-main {
+      width: 100%;
+      height: 100%;
+    }
+  }
 </style>
