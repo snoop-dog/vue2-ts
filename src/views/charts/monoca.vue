c9efb94a79b63de6fc503dcbfac6c89c5f24ef77
qilinjie <qilinjie1991@outlook.com>
2020-09-28 18:37:37 +0800

    update: monaco editor

--- src/views/charts/monoca.vue
+++ src/views/charts/monoca.vue
@@ -1,27 +1,30 @@
 <template>
   <el-container class="monoca-container">
-    <el-main id="monoca" class="monoca-main"></el-main>
+    <el-main id="monaco" style="width: 800px; height: 80%" class="monoca-main"></el-main>
   </el-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.main.js'
-import { component } from 'vue/types/umd'
 
 @Component
 export default class Chart extends Vue {
-  public monoca
+  public monacoInstance
   public str = 'hello world'
 
-  created () {
-    // this.monoca = monaco.editor.create(
-    //   document.querySelector('#monoca'),
-    //   {
-    //     value: `console.log(${this.str})`,
-    //     language: 'typescript'
-    //   }
-    // )
+  public createMonacoInstance () {
+    this.monacoInstance = monaco.editor.create(
+      document.getElementById('monaco'),
+      {
+        value: `console.log(${this.str})`,
+        language: 'typescript'
+      }
+    )
+  }
+
+  mounted () {
+    this.createMonacoInstance()
   }
 }
 </script>
@@ -31,8 +34,9 @@ export default class Chart extends Vue {
     width: 100%;
     height: 100%;
     .monoca-main {
-      width: 100%;
-      height: 100%;
+      width: 50%;
+      height: 80%;
+      flex: unset;
     }
   }
 </style>
